0162a2dc99833e994dcdf02718c029fe2d7be8d3
Barrett17 <b.vitruvio@gmail.com>
2018-11-14 10:55:04 +0100

    TransactionSetBuilder: Add FundOutput and FundOutputs tests
    * Based on those from TransactionBuilder.

--- modules/wallet/transactionsetbuilder_test.go
+++ modules/wallet/transactionsetbuilder_test.go
@@ -3,11 +3,135 @@ package wallet
 import (
 	"testing"
 	"time"
-	"unsafe"
 
 	"github.com/HyperspaceApp/Hyperspace/modules"
 	"github.com/HyperspaceApp/Hyperspace/types"
 )
+
+func TestFundOutputTransactionSet(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	wt, err := createWalletTester(t.Name(), &modules.ProductionDependencies{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer wt.closeWt()
+
+	b, err := wt.wallet.StartTransactionSet()
+	if err != nil {
+		t.Fatal(err)
+	}
+	uc1, err := wt.wallet.NextAddress()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	amount1 := types.NewCurrency64(1000)
+	output1 := types.SiacoinOutput{
+		Value:      amount1,
+		UnlockHash: uc1.UnlockHash(),
+	}
+	minerFee := types.NewCurrency64(750)
+
+	// Wallet starts off with large inputs from mining blocks, larger than our
+	// combined outputs and miner fees
+	err = b.FundOutput(output1, minerFee)
+	if err != nil {
+		t.Fatal(err)
+	}
+	unfinishedTxn, _ := b.View()
+
+	// Here we should have 3 outputs, the two specified plus a refund
+	if len(unfinishedTxn.SiacoinOutputs) != 2 {
+		t.Fatal("incorrect number of outputs generated")
+	}
+	if len(unfinishedTxn.MinerFees) != 1 {
+		t.Fatal("miner fees were not generated but should have been")
+	}
+	if unfinishedTxn.MinerFees[0].Cmp(minerFee) != 0 {
+		t.Fatal("miner fees were not generated but should have been")
+	}
+
+	// General construction seems ok, let's sign and submit it to the tpool
+	txSet, err := b.Sign(true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// If the tpool accepts it, everything looks good
+	err = wt.tpool.AcceptTransactionSet(txSet)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestFundOutputsTransactionSet(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	wt, err := createWalletTester(t.Name(), &modules.ProductionDependencies{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer wt.closeWt()
+
+	b, err := wt.wallet.StartTransactionSet()
+	if err != nil {
+		t.Fatal(err)
+	}
+	uc1, err := wt.wallet.NextAddress()
+	if err != nil {
+		t.Fatal(err)
+	}
+	uc2, err := wt.wallet.NextAddress()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	amount1 := types.NewCurrency64(1000)
+	amount2 := types.NewCurrency64(2000)
+	output1 := types.SiacoinOutput{
+		Value:      amount1,
+		UnlockHash: uc1.UnlockHash(),
+	}
+	output2 := types.SiacoinOutput{
+		Value:      amount2,
+		UnlockHash: uc2.UnlockHash(),
+	}
+	minerFee := types.NewCurrency64(750)
+
+	// Wallet starts off with large inputs from mining blocks, larger than our
+	// combined outputs and miner fees
+	err = b.FundOutputs([]types.SiacoinOutput{output1, output2}, minerFee)
+	if err != nil {
+		t.Fatal(err)
+	}
+	unfinishedTxn, _ := b.View()
+
+	// Here we should have 3 outputs, the two specified plus a refund
+	if len(unfinishedTxn.SiacoinOutputs) != 3 {
+		t.Fatal("incorrect number of outputs generated")
+	}
+	if len(unfinishedTxn.MinerFees) != 1 {
+		t.Fatal("miner fees were not generated but should have been")
+	}
+	if unfinishedTxn.MinerFees[0].Cmp(minerFee) != 0 {
+		t.Fatal("miner fees were not generated but should have been")
+	}
+
+	// General construction seems ok, let's sign and submit it to the tpool
+	txSet, err := b.Sign(true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// If the tpool accepts it, everything looks good
+	err = wt.tpool.AcceptTransactionSet(txSet)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
 
 // TestTransactionsFillWallet mines many blocks and checks that the wallet's
 // outputs are built in a manner to fill the txset
